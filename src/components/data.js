7c10c45407a58a9db8bd83539981427605f7d469
Vitor Hugo G. da Silva <vitorhugo.programador@gmail.com>
2021-09-18 18:04:58 -0300

    (fix): alterando cores dos textos do botão e do card items

--- src/components/data.js
+++ src/components/data.js
@@ -69,7 +69,7 @@ export const portifolio = {
                  },
                  { 
                     title: "Batiman",
-                    img: "https://images.unsplash.com/photo-1601645191163-3fc0d5d64e35?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80",
+                    img: "https://images.unsplash.com/photo-1497124401559-3e75ec2ed794?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
                     link: "https://www.instagram.com/p/CABFRR3nRqW/"
                }
             ]
