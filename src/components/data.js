fb8659201350afde8b7c3bd072538d14bf957794
Vitor Hugo G. da Silva <vitorhugo.programador@gmail.com>
2021-09-19 14:56:22 -0300

    (fix): modificando a animação do container cards

--- src/components/data.js
+++ src/components/data.js
@@ -1,79 +1,78 @@
 export const portifolio = {
-    background: "https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    foto_perfil: "https://avatars.githubusercontent.com/u/62728505?v=4",
-    nome : "Vitor Hugo",
-    profissao: "Desenvolvedor Front-end",
+  background:
+    "https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+  foto_perfil: "https://avatars.githubusercontent.com/u/62728505?v=4",
+  nome: "Vitor Hugo",
+  profissao: "Desenvolvedor Front-end",
 
-    redes: [
-        {
-            logo: "https://cdn-icons-png.flaticon.com/64/1384/1384023.png",
-            nome: "Whatsapp",
-            link: "https://api.whatsapp.com/send?phone=5511949120546"
-        },
-        {
-            logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
-            nome: "Github",
-            link: "https://github.com/VitorHugoG"
-        },
-        {
-            logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg",
-            nome: "Linkedin",
-            link: "https://www.linkedin.com/in/vitor-hugo-730044152/"
-        }
-    ],
-    
-    abas: [
-         {
-             title: "✍️ estudo ",
-             posts: [
-                  { 
-                      title:"Java",
-                      img: "https://images.unsplash.com/photo-1588239034647-25783cbfcfc1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80",
-                      link: "ggg"
-                  },
-                  { 
-                    title: "Javascript",
-                    img: "https://images.unsplash.com/photo-1543966888-7c1dc482a810?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-                    link: "aaa"
-                  },
-                  { 
-                    title: "CSS",
-                    img: "https://images.unsplash.com/photo-1505685296765-3a2736de412f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-                    link: "bbb"
-                  }
-             ]
-         },
-         {
-            title: "💻 projetos",
-            posts: [
-                 { 
-                     title: "Qfome",
-                     img: "https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-                     link: "https://github.com/VitorHugoG/QFome-1"
-                 },
-                 { 
-                   title: "Aviva",
-                   img: "https://images.unsplash.com/photo-1608778581110-bba5c3bde272?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-                   link: "https://github.com/VitorHugoG/Aviva"
-               }
-             
-            ]
-        },
-        {
-            title: "🎨 artes",
-            posts: [
-                 { 
-                     title: "Venus de Milo",
-                     img: "https://fetchnow.herokuapp.com/media?id=aHR0cHM6Ly9pbnN0YWdyYW0uZmFzcjEtMS5mbmEuZmJjZG4ubmV0L3YvdDUxLjI4ODUtMTUvZTM1LzE3NjI3ODE5OF8yMDgzMTc4MjQwNzg4ODlfODk2NDUxMDQ1MTE1MzIwOTM5Nl9uLmpwZz9fbmNfaHQ9aW5zdGFncmFtLmZhc3IxLTEuZm5hLmZiY2RuLm5ldCZfbmNfY2F0PTEwOSZfbmNfb2hjPWdZb000c2thTDdZQVg4SXlNeUwmZWRtPUFBQkJ2alVCQUFBQSZjY2I9Ny00Jm9oPWFmOTZjZTRmNzY4NjZiOGYzZjRiYjJiNzQwZWY1ZGZhJm9lPTYxNEIwMkE2Jl9uY19zaWQ9ODNkNjAz",
-                     link: "https://www.instagram.com/p/CN6KFMGLp9T/"
-                 },
-                 { 
-                    title: "Batiman",
-                    img: "https://images.unsplash.com/photo-1497124401559-3e75ec2ed794?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-                    link: "https://www.instagram.com/p/CABFRR3nRqW/"
-               }
-            ]
-        }
-    ]
+  redes: [
+    {
+      logo: "https://cdn-icons-png.flaticon.com/64/1384/1384023.png",
+      nome: "Whatsapp",
+      link: "https://api.whatsapp.com/send?phone=5511949120546",
+    },
+    {
+      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+      nome: "Github",
+      link: "https://github.com/VitorHugoG",
+    },
+    {
+      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg",
+      nome: "Linkedin",
+      link: "https://www.linkedin.com/in/vitor-hugo-730044152/",
+    },
+  ],
 
-}
+  abas: [
+    {
+      title: "✍️ estudo ",
+      posts: [
+        {
+          title: "Java",
+          img: "https://images.unsplash.com/photo-1588239034647-25783cbfcfc1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80",
+          link: "ggg",
+        },
+        {
+          title: "Javascript",
+          img: "https://images.unsplash.com/photo-1543966888-7c1dc482a810?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+          link: "aaa",
+        },
+        {
+          title: "CSS",
+          img: "https://images.unsplash.com/photo-1505685296765-3a2736de412f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+          link: "bbb",
+        },
+      ],
+    },
+    {
+      title: "💻 projetos",
+      posts: [
+        {
+          title: "Qfome",
+          img: "https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+          link: "https://github.com/VitorHugoG/QFome-1",
+        },
+        {
+          title: "Aviva",
+          img: "https://images.unsplash.com/photo-1608778581110-bba5c3bde272?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          link: "https://github.com/VitorHugoG/Aviva",
+        },
+      ],
+    },
+    {
+      title: "🎨 artes",
+      posts: [
+        {
+          title: "Venus de Milo",
+          img: "https://fetchnow.herokuapp.com/media?id=aHR0cHM6Ly9pbnN0YWdyYW0uZmFzcjEtMS5mbmEuZmJjZG4ubmV0L3YvdDUxLjI4ODUtMTUvZTM1LzE3NjI3ODE5OF8yMDgzMTc4MjQwNzg4ODlfODk2NDUxMDQ1MTE1MzIwOTM5Nl9uLmpwZz9fbmNfaHQ9aW5zdGFncmFtLmZhc3IxLTEuZm5hLmZiY2RuLm5ldCZfbmNfY2F0PTEwOSZfbmNfb2hjPWdZb000c2thTDdZQVg4SXlNeUwmZWRtPUFBQkJ2alVCQUFBQSZjY2I9Ny00Jm9oPWFmOTZjZTRmNzY4NjZiOGYzZjRiYjJiNzQwZWY1ZGZhJm9lPTYxNEIwMkE2Jl9uY19zaWQ9ODNkNjAz",
+          link: "https://www.instagram.com/p/CN6KFMGLp9T/",
+        },
+        {
+          title: "Batiman",
+          img: "https://images.unsplash.com/photo-1497124401559-3e75ec2ed794?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+          link: "https://www.instagram.com/p/CABFRR3nRqW/",
+        },
+      ],
+    },
+  ],
+};
